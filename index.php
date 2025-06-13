534c22e287d69b598c3f83e89d2edd83c0c61dfe
bhupesh <premad.bhupesh@gmail.com>
2025-06-13 18:02:05 +0530

    print base url

--- index.php
+++ index.php
@@ -64,7 +64,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                                <?php 
+// echo 'BASE_URL: ' . BASE_URL;
+                                
+                                ?>
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
