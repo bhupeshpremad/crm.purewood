93756325592c164035490f283ca9310f9d3e8d99
bhupesh <premad.bhupesh@gmail.com>
2025-06-13 18:50:34 +0530

    change base url

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?php echo BASE_URL; ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-gradient-primary">
@@ -146,7 +146,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="<?php echo BASE_URL; ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?php echo BASE_URL; ?>js/sb-admin-2.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/js/sb-admin-2.min.js"></script>
 
 </body>
 
